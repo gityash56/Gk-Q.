4d0d327e55f64d1e296b2d41b15843f8e6a9d6d1
YASH SHAH <90525149+gityash56@users.noreply.github.com>
2022-10-01 13:30:48 +0530

    Update gitsubmit.py

--- gitsubmit.py
+++ gitsubmit.py
@@ -8,6 +8,7 @@ Original file is located at
 
 **Create a program that asks the user to enter their name and their age. Print out a message that tells how many years they have to be 100 years old.**
 """
+
 1)
 import datetime
 Name = input("Enter your name: ")
@@ -20,15 +21,10 @@ name = input("Enter Your Name:")
 age = int(input("Enter Your Age:"))
 print("You will Turn 100 in : ",100-age+2022) #Logic =>  100 - 21 (current_Age) = 79 (Final_Age)  +  2022 (current_Year)
 
+
 3)
 from datetime import datetime
 name = input('What is your name? \n')
 age = int(input('How old are you? \n'))
 hundred = int((100-age) + datetime.now().year)
 print ('Hello %s. You are %s years old. You will turn 100 years old in %s.' % (name, age, hundred))
-
-#repeat = int(input('Select a random number: \n'))
-#stmt = ('Hello %s. You are %s years old. You will turn 100 years old in %s. \n' % (name, age, hundred))
-#print (stmt*repeat)
-
-
